b0610da201094e771653f92af8022d1b9e5ff1bb
suppaman10000 <160000588+suppaman10000@users.noreply.github.com>
2024-03-06 15:40:49 +0900

    課題15追加

---
+++ kadai_015/kadai_015.py
@@ -0,0 +1,16 @@
+
+class Human:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+    
+
+  def printinfo(self):
+    print(self.name)
+    print(self.age)
+    
+human = Human("侍太郎", 20)
+
+human.printinfo()
+    
+    
